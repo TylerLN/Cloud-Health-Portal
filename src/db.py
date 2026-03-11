2ade483c3808b9bf2b94e86ca0a9b1acf25a8919
Jake Nellesen <jnellesen@csu.fullerton.edu>
2026-03-10 22:57:42 -0700

    encrypting
    db will have tls, client will force tls, change db config for dev

--- src/db.py
+++ src/db.py
@@ -13,12 +13,39 @@ class db_conn:
         username: str = "postgres",
         passfile: str = "./pgpass",
     ):
-        self.pool = await asyncpg.create_pool(host=host, port=port, username=username)
+        self.pool = await asyncpg.create_pool(
+            host=host, port=port, username=username, ssl="require"
+        )
         async with self.pool.acquire() as con:
-            await con.execute('''
-                CREATE TABLE accounts (
-                    id serial PRIMARY KEY,
-                    name VARCHAR (255) NOT NULL
-                )
-            ''')
+            await con.execute(
+                """
+                    CREATE EXTENSION
+                    IF NOT EXISTS
+                    pgcrypto;
+                """
+            )
+            await con.execute(
+                """
+                    CREATE TABLE
+                    IF NOT EXISTS
+                    accounts (
+                        id serial PRIMARY KEY,
+                        username TEXT NOT NULL UNIQUE,
+                        password TEXT NOT NULL,
+                    );
+                """
+            )
+            await con.execute(
+                """
+                    CREATE TABLE
+                    IF NOT EXISTS
+                    personal_info (
+                        account_id int NOT NULL REFRENCES accounts(id),
+                        first_name VARCHAR (255) NOT NULL,
+                        middle_name VARCHAR (255),
+                        last_name VARCHAR (255)
+                    );
+                """
+            )
 
+    async def 
