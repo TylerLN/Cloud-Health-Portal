026e17c41fffa37882ce2199d89aa93fbb2ec4ae
Jake-P-N <jprcnellesen@gmail.com>
2026-03-10 19:59:51 -0700

    implement connection pool

--- src/db.py
+++ src/db.py
@@ -1,2 +1,24 @@
 import asyncio
+import ssl
+
 import asyncpg
+
+
+class db_conn:
+
+    def __init__(
+        self,
+        host: str | list[str] = None,
+        port: int | list[port] = None,
+        username: str = "postgres",
+        passfile: str = "./pgpass",
+    ):
+        self.pool = await asyncpg.create_pool(host=host, port=port, username=username)
+        async with self.pool.acquire() as con:
+            await con.execute('''
+                CREATE TABLE accounts (
+                    id serial PRIMARY KEY,
+                    name VARCHAR (255) NOT NULL
+                )
+            ''')
+
