b19c3b97625356fb26b567cf96c37532a297772b
TylerLN <tylerlngu@csu.fullerton.edu>
2026-04-11 14:15:30 -0700

    Fixed comments

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -28,11 +28,13 @@ resource "aws_iam_policy" "s3_access_policy" {
   name        = "s3-access-policy"
   description = "Allow EC2 backend to securely access S3 bucket"
 
+  // (1) lists the bucket itself, required for accessing objects
+  // (2) allows access to objects within the bucket
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Sid    = "ListBucket" // lists the bucket itself, required for accessing objects
+        Sid    = "ListBucket" 
         Effect = "Allow"
         Action = [
           "s3:ListBucket"
@@ -40,7 +42,7 @@ resource "aws_iam_policy" "s3_access_policy" {
         Resource = aws_s3_bucket.file_transfers.arn
       },
       {
-        Sid    = "ObjectAccess" // allows access to objects within the bucket
+        Sid    = "ObjectAccess"
         Effect = "Allow"
         Action = [
           "s3:GetObject",
